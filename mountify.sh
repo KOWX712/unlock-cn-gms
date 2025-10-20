e950e08014f4ad53c2421aa572b6ef1f8d258405
KOWX712 <leecc0503@gmail.com>
2025-10-20 15:28:17 +0800

    sync mountify standalone to 019a216
    ref: https://github.com/backslashxx/mountify/commit/019a216866fae4ffc0c65adcf9a98228dc67a87d
    
    Co-Authored-By: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- mountify.sh
+++ mountify.sh
@@ -8,15 +8,25 @@
 # This is free software; you can redistribute it and/or modify it under the terms of The Unlicense.
 PATH=/data/adb/ap/bin:/data/adb/ksu/bin:/data/adb/magisk:$PATH
 MODDIR="${0%/*}"
+
+# you can mimic vendor mounts like, my_bigball, vendor_dklm, mi_ext
+# whatever. use what you want. provided here is just an example
 FAKE_MOUNT_NAME="unlock_cn_gms"
+
+# mount device name
+# you can use APatch, KSU or magisk here to let a zygisk provider unmount
+# otherwise, just use "overlay" as default
+MOUNT_DEVICE_NAME="overlay"
+
+# you can also use random characters whatever, but this might be a bad meme
+# as we are trying to mimic a vendor mount, but its here if you want
+# uncomment to use
+# FAKE_MOUNT_NAME="$(cat /dev/urandom | tr -dc A-Za-z0-9 | head -c 10)"
 
 # susfs usage is not required but we can use it if its there.
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
-if command -v ${SUSFS_BIN} > /dev/null 2>&1; then
-    mountify_use_susfs=1
-else
-    mountify_use_susfs=0
-fi
+# set to 1 to enable
+mountify_use_susfs=0
 
 # separate shit with lines
 IFS="
@@ -27,8 +37,18 @@ targets="odm
 product
 system_ext
 vendor
+mi_ext
 my_bigball
-my_heytap"
+my_carrier
+my_company
+my_engineering
+my_heytap
+my_manifest
+my_preload
+my_product
+my_region
+my_reserve
+my_stock"
 
 # functions
 
@@ -36,7 +56,7 @@ my_heytap"
 controlled_depth() {
 	if [ -z "$1" ] || [ -z "$2" ]; then return ; fi
 	for DIR in $(ls -d $1/*/ | sed 's/.$//' ); do
-		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:$2$DIR" overlay "$2$DIR"
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:$2$DIR" "$MOUNT_DEVICE_NAME" "$2$DIR"
 		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "$2$DIR"
 	done
 }
@@ -44,8 +64,8 @@ controlled_depth() {
 # handle single depth (/system/bin, /system/etc, et. al)
 single_depth() {
 	for DIR in $( ls -d */ | sed 's/.$//' | grep -vE "(odm|product|system_ext|vendor)$" 2>/dev/null ); do
-		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/system/$DIR" overlay "/system/$DIR"
-		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "/system/$DIR"
+		busybox mount -t overlay -o "lowerdir=$(pwd)/$DIR:/system/$DIR" "$MOUNT_DEVICE_NAME" "/system/$DIR"
+		[ $mountify_use_susfs = 1 ] && ${SUSFS_BIN} add_sus_mount "$2$DIR"
 	done
 }
 
@@ -57,11 +77,14 @@ else
 fi
 
 # routine start
+echo "mountify/standalone: start!" >> /dev/kmsg
 
 # make sure $MODDIR/skip_mount exists!
 # this way manager won't mount it
 # as we handle the mounting ourselves
 [ ! -f $MODDIR/skip_mount ] && touch $MODDIR/skip_mount
+# mountify 131 added this
+# this way mountify wont remount this module
 [ ! -f $MODDIR/skip_mountify ] && touch $MODDIR/skip_mountify
 
 # this is a fast lookup for a writable dir
@@ -71,19 +94,32 @@ fi
 
 # make sure fake_mount name does not exist
 if [ -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then 
+	echo "mountify/standalone: folder with name $FAKE_MOUNT_NAME already exists!" >> /dev/kmsg
 	exit 1
 fi
 
-BASE_DIR="$MODDIR/system"
+# create our folder
+mkdir -p "$MNT_FOLDER/$FAKE_MOUNT_NAME"
 
-# copy it
-cd "$MNT_FOLDER" && cp -Lrf "$BASE_DIR" "$FAKE_MOUNT_NAME"
+# mount our own tmpfs
+echo "mountify/standalone: mounting $(realpath "$MNT_FOLDER/$FAKE_MOUNT_NAME")" >> /dev/kmsg
+busybox mount -t tmpfs tmpfs "$(realpath "$MNT_FOLDER/$FAKE_MOUNT_NAME")"
 
 # then we make sure its there
 if [ ! -d "$MNT_FOLDER/$FAKE_MOUNT_NAME" ]; then
 	echo "standalone lol exit"
 	exit 1
 fi
+
+# create placeholder
+touch "$MNT_FOLDER/$FAKE_MOUNT_NAME/placeholder"
+
+# our base directory on magic mount
+BASE_DIR="$MODDIR/system"
+
+# copy over our files: follow symlinks, recursive, force.
+echo "mountify/standalone: processing $MODDIR" >> /dev/kmsg
+cp -Lrf "$BASE_DIR"/* "$MNT_FOLDER/$FAKE_MOUNT_NAME"
 
 # go inside
 cd "$MNT_FOLDER/$FAKE_MOUNT_NAME"
@@ -119,5 +155,9 @@ for folder in $targets ; do
 		controlled_depth "$folder" "/"
 	fi
 done
+
+# unmount the tmpfs we created
+echo "mountify/standalone: unmounting $(realpath "$MNT_FOLDER/$FAKE_MOUNT_NAME")" >> /dev/kmsg
+busybox umount -l "$(realpath "$MNT_FOLDER/$FAKE_MOUNT_NAME")"
 
 # EOF
